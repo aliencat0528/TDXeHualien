89180aeeb058890662d0662fc519d64f0a161f32
Angel <angel0528.chang@gmail.com>
2022-03-02 14:20:13 +0800

    test locevetype3

--- tdxDemo/views.py
+++ tdxDemo/views.py
@@ -45,7 +45,7 @@ def callback(request):
                 #line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
                 # if isinstance(event.message,LocationMessage):
                 #     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
+
         return HttpResponse()
 
     else:
