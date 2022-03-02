668afddea23fcafb219d2320f0f17a79a2b1cb4f
Angel <angel0528.chang@gmail.com>
2022-03-02 15:07:50 +0800

    test webhookmsg3

--- tdxDemo/views.py
+++ tdxDemo/views.py
@@ -45,7 +45,7 @@ def callback(request):
                 #line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
             # if isinstance(event,PostbackEvent):
             #     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.text))
+            #line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.text))
         return HttpResponse()
 
     else:
